253f5b93e7316aaab53d228a74bb56e242854b86
stevensuting <steven.suting@gmail.com>
2023-05-11 22:44:11 +0530

    Create superset_config_docker.py

---
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -0,0 +1,18 @@
+# Specify the App icon
+APP_ICON = "/static/assets/images/aegus-logo.png"
+
+
+
+# custom color palette
+EXTRA_CATEGORICAL_COLOR_SCHEMES = [
+    {
+        "id": 'aegus-custom-colors',
+        "description": '',
+        "label": 'Aegus',
+        "isDefault": True,
+        "colors":
+         ['#006699', '#009DD9', '#5AAA46', '#44AAAA', '#DDAA77', '#7799BB', '#88AA77',
+         '#552288', '#5AAA46', '#CC7788', '#EEDD55', '#9977BB', '#BBAA44', '#DDCCDD']
+    }]
+
+ 
